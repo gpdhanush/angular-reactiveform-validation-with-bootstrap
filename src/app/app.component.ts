45bb29b6383d714e3576e11603570a4dbcb30c34
gpdhanush <agprakash406@gmail.com>
2020-03-11 15:10:41 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  maxDate = new Date();
+  myDob: any;
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createRegisterForm();
+  }
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.compose([Validators.required])],
+      emailId: ['', Validators.compose([Validators.required,
+      Validators.pattern('^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])],
+      mobile: ['', Validators.compose([Validators.required, Validators.maxLength(10)])],
+      dob: ['', Validators.compose([Validators.required])]
+    });
+  }
+  registerFormSubmit(value) {
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import {BsDatepickerModule, DatepickerModule} from 'ngx-bootstrap';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule,BrowserAnimationsModule,
+  BsDatepickerModule.forRoot(),DatepickerModule.forRoot()],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
