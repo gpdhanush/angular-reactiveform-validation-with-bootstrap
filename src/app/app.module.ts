a729fee87979fbbca45cb365c358d2713ea8daf1
gpdhanush <agprakash406@gmail.com>
2020-03-11 18:00:35 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import {BsDatepickerModule, DatepickerModule} from 'ngx-bootstrap';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule,BrowserAnimationsModule,
   BsDatepickerModule.forRoot(),DatepickerModule.forRoot()],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
